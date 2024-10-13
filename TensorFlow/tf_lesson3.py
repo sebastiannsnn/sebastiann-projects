84a93456c0242f047fa029355c01d42fd115eb53
sebastiann <sebastiann.snn@gmail.com>
2024-10-13 21:16:45 +0100

    Add files via upload

--- TensorFlow/tf_lesson3.py
+++ TensorFlow/tf_lesson3.py
@@ -19,3 +19,18 @@ def preprocess(x):
 processed_dataset = dataset.map(preprocess)
 for element in processed_dataset:
     print(element.numpy())
+
+    import tensorflow as tf
+
+# Create a dataset
+dataset1 = tf.data.Dataset.from_tensor_slices([10, 20, 30, 40, 50])
+
+# Define a preprocessing function
+def preprocess(x):
+    return x + 10
+
+# Chain preprocessing, shuffling, and batching
+pipeline = dataset1.map(preprocess).shuffle(buffer_size=5).batch(2)
+
+for batch in pipeline:
+    print(batch.numpy())
